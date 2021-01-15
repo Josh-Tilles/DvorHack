d68a853b5a6b04e4d2dd74968af59695ad5a1305
Roland Kaufmann <roland@kaufmann.no>
2021-01-15 20:46:05 +0000

    (release 1.2.12)

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e -x
 LAYOUT="Programmer Dvorak"
 LANGUAGE=en
 VERSION=1.2
-COPYRIGHT="Copyright 1997--2019 (c) Roland Kaufmann"
+COPYRIGHT="Copyright 1997--2020 (c) Roland Kaufmann"
 
 # copy files from where this script is located
 pushd $(dirname $0)
